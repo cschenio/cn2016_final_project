eb4f9cc4f311cf400cac8c0b000cb44c9a236c58
littlehug <b03902081@csie.ntu.edu.tw>
2017-01-06 11:38:37 +0800

    fix routing bug

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './models/user'
 
 set :database, {adapter: "sqlite3", database: "cnline.sqlite3"}
 
+enable :sessions
 
 get '/' do
   erb :index
@@ -13,7 +14,7 @@ end
 ### User START ###
 
 get '/signup' do
-  erb :'/signup'
+  erb :'/signup/index'
 end
 
 post '/signup' do
@@ -26,23 +27,40 @@ post '/signup' do
 end
 
 get '/login' do
-  erb :'/login'
+  erb :'/login/index'
 end
 
 post '/login' do
   puts params
-  @user = User.find_by(username: params["username"],
-                      password: params["password"])
-  session[:id] = @user.id
+  @user = User.find_by(:username => params["username"],
+                      :password => params["password"])
+
+  if @user.nil?
+    puts "User not found"
+    redirect '/error'
+  else
+    session[:id] = @user.id
+    redirect '/users'
+  end
 end
 
 get '/logout' do
   session.clear
+  puts "logout"
+  redirect '/'
 end
 
 get '/users' do
-  @user = User.find(session[:id])
-  erb :'/users'
+  if session[:id]
+    @user = User.find(session[:id])
+    erb :'/users/index'
+  else
+    redirect '/error'
+  end
+end
+
+get '/error' do
+  erb :'error'
 end
 
 ### User END ###

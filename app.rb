0c0860d7f30076f5ab5cdb18a4f1931d6bdbcc99
qitar888 <qitar888@linux-rurf.suse>
2017-01-06 09:54:51 +0800

    Make dummy user

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './models/user'
+
+set :database, {adapter: "sqlite3", database: "cnline.sqlite3"}
 
 get '/' do
+  @user = User.new
   erb :index
 end

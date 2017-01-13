e6d12aacd8b1cafe0b7718c48342acca54f7d744
littlehug <b03902081@csie.ntu.edu.tw>
2017-01-13 08:45:28 +0800

    encode url for escape symbol

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ require './controller_concerns/permission_authable'
 include PermissionAuthable
 
 # File transfer
+require 'uri'
 require 'fileutils'
 require_relative './config' # contains file_path
 

d8d9e21e3c1e2697adbfa6ee1b96d67d84231d1e
littlehug <b03902081@csie.ntu.edu.tw>
2017-01-14 03:19:30 +0800

    validates successful for online user has_file

--- models/online.rb
+++ models/online.rb
@@ -2,6 +2,5 @@ class Online < ActiveRecord::Base
 
   belongs_to :user
   validates :user, presence: true 
-  #validates :has_file, presence: true
-
+  validates :has_file, inclusion: { in: [ true, false ] }
 end

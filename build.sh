c85ca839ecc331b1bd532e586aa76badba44fcee
Chia-sheng Chen <qitar888@gmail.com>
2017-01-14 19:05:46 +0800

    Put files into src

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+cd src
+gem install bundler
 bundle install
 bundle exec rake db:create
 bundle exec rake db:migrate
--- config.rb
+++
@@ -1,3 +0,0 @@
-module Path
-  FILE_PATH = '/Users/Chang/github/cn2016_final_project/uploads'
-end
